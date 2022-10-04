1339462ffe0ba69f889ee628f8e356c2a722b68d
hernany <hernanytec777@gmail.com>
2022-10-04 00:00:47 -0300

    feat: add comment component

---
+++ src/components/Comment.tsx
@@ -0,0 +1,40 @@
+import { Trash, ThumbsUp } from "phosphor-react";
+
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https:github.com/hernanytec.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Hernany Costa</strong>
+              <time
+                title="03 de Outubro às 23:08h"
+                dateTime="2022-03-10 23:08:00"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
